70780492ce10bee54485326f214949b081d51d1e
Edson-Padilha <60894182+Edson-Padilha@users.noreply.github.com>
2021-06-04 16:17:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+function clicou(){
+    alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://edson-padilha.github.io");
+    //window.location.href = "https://edson-padilha.github.io";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+
+function funcaochange(elemento){
+    console.log(elemento.value)
+}
+/*function validarIdade(idade){
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+var idade = prompt("Qual sua idade? ");
+validarIdade (idade)
+console.log(validar);*/
+
+/*function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert("Vai Japão", "Japão","Brasil");*/
+
+/*function soma(n1 , n2){
+    return n1 + n2;
+}
+alert(soma(5, 10));*/
+
+/*var d = new Date();
+alert(d);
+alert(d.getDate()+1);
+alert(d.getFullYear());
+alert(d.getHours());
+alert(d.getUTCMonth());
+alert(d.getTime());*/
+
+/*var count;
+for(count=0; count<=5; count++){
+    alert(count);
+    console.log(count);
+};*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};*/
+
+/*var idade = prompt("Qual sua idade? ");
+//var idade = 16;
+if(idade >= 18){
+alert("Maior de idade");
+}
+
+else{
+    alert("Menor de idade");
+}*/
+
+
+/*var nomes = [{Nome:"Edson", Sobrenome:"Padilha" },
+{Nome:"Isadora",Sobrenome:"Padilha"}];
+console.log(nomes);*/
+
+
+/* var lista = ["Python", "HTML", "JavaScript"];
+alert(lista[1]);
+lista.push("Java");
+console.log(lista);*/
+
+/*var nome = "Edson Padilha";
+var idade = 39;
+var frase = "Sou estudante Super Dev"
+alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade * 2);
+console.log(frase);
+console.log(frase.replace("Super Dev", "Front-End"));
+alert(frase.replace("Super Dev", "Front-End"));*/
+
+
